8ae29317cf0ed34237a370329ac03d68caa29e1a
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2019-10-05 14:34:24 -0300

    Configuração inicial chat

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        Musonza\Chat\ChatServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
@@ -227,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Chat' => Musonza\Chat\Facades\ChatFacade::class,
     ],
 
 ];
